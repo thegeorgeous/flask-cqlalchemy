fdf55606b5b6617f9249dfb6c6da788b7fc444e3
George <incredibleguy88@gmail.com>
2015-06-12 01:30:12 +0530

    Modified setup.py for packaging and added .pyc to .gitignore

--- setup.py
+++ setup.py
@@ -11,13 +11,14 @@ from setuptools import setup
 
 setup(
     name='Flask-CQLAlchemy',
-    version='0.1',
+    version='0.1.0.dev1',
     url='http://thegeorgeous.github.io/Flask-CQLAlchemy',
     license='BSD',
     author='George',
     author_email='iamgeorgethomas@gmail.com',
     description='Flask-CQLAlchemy handles connections to Cassandra clusters through the cqlengine',
     long_description=__doc__,
+    keywords='cassandra cqlengine flask',
     packages=['flask_cqlalchemy'],
     zip_safe=False,
     include_package_data=True,
@@ -28,11 +29,13 @@ setup(
         'blist'
     ],
     classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
         'Environment :: Web Environment',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
-        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
+        'Framework :: Flask',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
