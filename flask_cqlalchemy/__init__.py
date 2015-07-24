2afcc911c8007b71a0aae681995243894749b5e8
George <incredibleguy88@gmail.com>
2015-07-24 18:40:22 +0530

    Added set_keyspace functionality

--- flask_cqlalchemy/__init__.py
+++ flask_cqlalchemy/__init__.py
@@ -9,7 +9,7 @@ flask_cqlalchemy
 from cassandra.cqlengine import connection
 from cassandra.cqlengine.management import sync_table, create_keyspace_simple
 from cassandra.cqlengine import columns
-from cassandra.cqlengine.models import Model
+from cassandra.cqlengine import models
 
 try:
     from flask import _app_ctx_stack as stack
@@ -27,7 +27,7 @@ class CQLAlchemy(object):
     def __init__(self, app=None):
         """Constructor for the class"""
         self.columns = columns
-        self.Model = Model
+        self.Model = models.Model
         self.app = app
         self.sync_table = sync_table
         if app is not None:
@@ -40,7 +40,7 @@ class CQLAlchemy(object):
         the Cassandra cluster and creates a connection at startup.
         """
         self._hosts_ = app.config['CASSANDRA_HOSTS']
-        self._default_keyspace_ = app.config['CASSANDRA_KEYSPACE']
+        self._keyspace_ = app.config['CASSANDRA_KEYSPACE']
         consistency = app.config.get('CASSANDRA_CONSISTENCY', 1)
         lazy_connect = app.config.get('CASSANDRA_LAZY_CONNECT', False)
         retry_connect = app.config.get('CASSANDRA_RETRY_CONNECT', False)
@@ -48,20 +48,21 @@ class CQLAlchemy(object):
 
         # Create a keyspace with a replication factor of 2
         # If the keyspace already exists, it will not be modified
+        if not self._hosts_ and self._keyspace_:
+            raise NoConfig("No Configuration options defined. At least CASSANDRA_HOSTS and CASSANDRA_CONSISTENCY must be supplied")
         connection.setup(self._hosts_,
-                         self._default_keyspace_,
+                         self._keyspace_,
                          consistency=consistency,
                          lazy_connect=lazy_connect,
                          retry_connect=retry_connect,
-                         **setup_kwargs
-                         )
+                         **setup_kwargs)
 
     def create_all(self):
         """Creates all the keyspaces and tables necessary for the app.
         If run from a shell, all defined models must be imported before
         this method is called
         """
-        create_keyspace_simple(self._default_keyspace_, 2)
+        create_keyspace_simple(self._keyspace_, 2)
         models = [cls for cls in self.Model.__subclasses__()]
         for model in models:
             sync_table(model)
@@ -73,3 +74,19 @@ class CQLAlchemy(object):
         models = [cls for cls in self.Model.__subclasses__()]
         for model in models:
             sync_table(model)
+
+    def set_keyspace(self, keyspace_name):
+        """ Changes keyspace for the current session if keyspace_name is
+        supplied. Ideally sessions exist for the entire duration of the
+        application. So if the change in keyspace is meant to be temporary,
+        this method must be called again without any arguments
+        """
+        if not keyspace_name:
+            keyspace_name = self._default_keyspace_
+        models.DEFAULT_KEYSPACE = keyspace_name
+        self._keyspace_ = keyspace_name
+
+
+class NoConfig(Exception):
+    """ Raised when CASSANDRA_HOSTS or CASSANDRA_KEYSPACE is not defined"""
+    pass
