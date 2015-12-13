3bdee22cba878dc4312de2ddf8ce54f6eab57567
George Thomas <iamgeorgethomas@gmail.com>
2015-12-14 02:34:54 +0530

    Add tests for sync_db and set_keyspace

---
+++ tests/test_cqlalchemy.py
@@ -0,0 +1,46 @@
+import unittest
+import uuid
+from flask import Flask
+from flask.ext.cqlalchemy import CQLAlchemy
+from cassandra.cqlengine.management import drop_keyspace, create_keyspace_simple
+from cassandra.cqlengine import models
+
+def make_user_model(db):
+    class User(db.Model):
+        uuid = db.columns.UUID(primary_key=True, default=uuid.uuid4)
+        username = db.columns.Text(index=True, required=False)
+
+    return User
+
+class BasicTestCase(unittest.TestCase):
+
+    def setUp(self):
+        app = Flask(__name__)
+        app.config['CASSANDRA_HOSTS'] = ['127.0.0.1']
+        app.config['CASSANDRA_KEYSPACE'] = "test1"
+        app.config['CASSANDRA_SETUP_KWARGS'] = {'protocol_version': 3}
+        db = CQLAlchemy(app)
+        self.User = make_user_model(db)
+        create_keyspace_simple("test1", 1)
+
+        self.app = app
+        self.db = db
+
+    def tearDown(self):
+        drop_keyspace("cqlengine")
+
+
+    def test_sync_db(self):
+        self.db.sync_db()
+        user = self.User.create(username="JohnDoe")
+        self.assertTrue(isinstance(user, self.User))
+        self.assertEqual(user.username, "JohnDoe")
+
+    def test_set_keyspace(self):
+        create_keyspace_simple("test2", 1)
+        self.db.set_keyspace('test2')
+        self.assertEqual(models.DEFAULT_KEYSPACE, "test2")
+        self.assertEqual(self.db._keyspace_, "test2")
+
+if __name__ == '__main__':
+    unittest.main()
